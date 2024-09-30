b9e17459a47c64d5c74700cca5165f231382ec55
Mattan Dor <mattan.dor1@gmail.com>
2024-09-30 13:15:01 +0300

    Added: Zoom API, randomly select type.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_stream';
-$plugin->version = 2024091800;
+$plugin->version = 2024093000;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v2.5';
