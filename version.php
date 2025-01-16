a400bf7bb0e64ae599f5e6867f295cb35a97a778
Mattan Dor <mattan.dor1@gmail.com>
2025-01-16 11:07:09 +0200

    Added: course tags retrieval and integration into stream object.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_stream';
-$plugin->version = 2024121600;
+$plugin->version = 2025011600;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.5';
+$plugin->release = 'v3.6';
 $plugin->dependencies = [
         'mod_stream' => 2024060700,
 ];
